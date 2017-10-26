e1d5187197d47765b774c2cc4cef0108f1ac5105
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2017-10-26 12:05:37 +0200

    Added new Tests for calculator with waiting 2.5 secs in each test

--- MainProject.Test/CalculatorTest.cs
+++ MainProject.Test/CalculatorTest.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
 
 namespace MainProject.Test
 {
     [TestClass]
     public class CalculatorTest
     {
+        [TestInitialize]
+        public void FakeInitialize()
+        {
+            Thread.Sleep(2500);
+        }
+
         [TestMethod]
         public void Add_OnlyPositives()
         {
@@ -14,6 +21,57 @@ namespace MainProject.Test
             double result = sut.Add(12, 14);
 
             Assert.AreEqual(26, result);
+        }
+
+        [TestMethod]
+        public void Add_PositiveAndNegatvie()
+        {
+            var sut = CreateSystemUnderTest();
+
+            double result = sut.Add(12, -14);
+
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        public void Add_OnlyNegative()
+        {
+            var sut = CreateSystemUnderTest();
+
+            double result = sut.Add(-12, -14);
+
+            Assert.AreEqual(-26, result);
+        }
+
+
+        [TestMethod]
+        public void Substract_OnlyPositives()
+        {
+            var sut = CreateSystemUnderTest();
+
+            double result = sut.Substract(14, 12);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Substract_PositiveAndNegative()
+        {
+            var sut = CreateSystemUnderTest();
+
+            double result = sut.Substract(12, -14);
+
+            Assert.AreEqual(26, result);
+        }
+
+        [TestMethod]
+        public void Substract_OnlyNegative()
+        {
+            var sut = CreateSystemUnderTest();
+
+            double result = sut.Substract(-12, -14);
+
+            Assert.AreEqual(2, result);
         }
 
         private Calculator CreateSystemUnderTest()
